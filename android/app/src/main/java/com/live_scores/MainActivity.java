c34a087fd1f055eb5822bb4b434563a13a7f4c9c
VladFedoseev7 <80280368+VladFedoseev7@users.noreply.github.com>
2021-08-26 12:17:05 +0300

    added splash screens

--- android/app/src/main/java/com/live_scores/MainActivity.java
+++ android/app/src/main/java/com/live_scores/MainActivity.java
@@ -1,13 +1,19 @@
 package com.live_scores;
 
 import com.facebook.react.ReactActivity;
-
+import org.devio.rn.splashscreen.SplashScreen; // import this
+import android.os.Bundle; 
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
+  }
   @Override
   protected String getMainComponentName() {
     return "Live_Scores";
