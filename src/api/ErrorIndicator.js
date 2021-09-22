b3d24fc79155fea17f3c26e94c10c67c5ae4ecfa
VladFedoseev7 <80280368+VladFedoseev7@users.noreply.github.com>
2021-09-22 11:58:37 +0300

    fixed issues with errorindicator

--- src/api/ErrorIndicator.js
+++ src/api/ErrorIndicator.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {ActivityIndicator, StyleSheet, Text, View, Alert} from 'react-native';
 
-const Error = () => (
-  <View style={styles.container}>
-    {Alert.alert('OOPS!', 'Error with uploading files', [
+const createButtonAlert = () =>
+  Alert.alert(
+    Alert.alert('OOPS!', 'Error with uploading files', [
       {text: 'Retry', onPress: () => console.log('closed')},
-    ])}
-  </View>
-);
+    ]),
+  );
+const Error = () => <View style={styles.container}>{createButtonAlert}</View>;
 
 const styles = StyleSheet.create({
   container: {
